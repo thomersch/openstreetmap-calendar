6b818427a0f7db8a485e0429c60828a6ac7a6ff7
Thomas Skowron <th@skowron.eu>
2019-08-13 13:27:12 +0200

    env file for production deployment

--- osmcal/settings.py
+++ osmcal/settings.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
