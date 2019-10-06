b421ff7f9b710fc328a24ff23d98bca9e7e9fc14
Thomas Skowron <th@skowron.eu>
2019-10-06 18:12:39 +0200

    event input form usability improvement

--- osmcal/forms.py
+++ osmcal/forms.py
@@ -8,10 +8,10 @@ from . import models
 class EventForm(forms.ModelForm):
     class Meta:
         model = models.Event
-        fields = ('name', 'start', 'end', 'whole_day', 'link', 'kind', 'location', 'description')
+        fields = ('name', 'whole_day', 'start', 'end', 'link', 'kind', 'location', 'description')
         widgets = {
             'location': LeafletWidget(),
             'start': DateTimeInput(attrs={'class': 'datepicker-flat'}),
-            'end': DateTimeInput(attrs={'class': 'datepicker-flat'}),
+            'end': DateTimeInput(attrs={'class': 'datepicker-flat', 'placeholder': 'optional'}),
             'link': TextInput(attrs={'placeholder': 'https://'})
         }
