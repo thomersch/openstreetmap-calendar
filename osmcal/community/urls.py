8b3fe8f31c75b5be1d7f84102d72ee704217ea1d
Thomas Skowron <th@skowron.eu>
2020-05-18 19:01:10 +0200

    community: join/leave communities

--- osmcal/community/urls.py
+++ osmcal/community/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('create/', views.CommunityCreate.as_view(), name='community-create'),
 
     path('<int:community_id>/', views.CommunityDetail.as_view(), name='community'),
+    path('<int:community_id>/join/', views.CommunityJoin.as_view(), name='community-join'),
+    path('<int:community_id>/leave/', views.CommunityLeave.as_view(), name='community-leave'),
 ]
--- osmcal/community/views.py
+++ osmcal/community/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 
 from . import forms
@@ -15,10 +18,12 @@ class CommunityList(View):
 
 
 class CommunityCreate(View):
+    @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
         form = forms.CommunityForm(label_suffix='')
         return render(request, 'osmcal/community/community_form.html', {'form': form})
 
+    @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         form = forms.CommunityForm(request.POST)
         form.is_valid()
@@ -39,3 +44,20 @@ class CommunityDetail(View):
             ctx['is_member'] = community.members.filter(id=request.user.id).count() > 0
 
         return render(request, 'osmcal/community/community_detail.html', ctx)
+
+
+class CommunityJoin(View):
+    @method_decorator(login_required)
+    def post(self, request, community_id):
+        community = get_object_or_404(Community, id=community_id)
+        community.members.add(request.user)
+        community.save()
+        return redirect(reverse('osmcal.community:community', args=[community_id]))
+
+
+class CommunityLeave(View):
+    @method_decorator(login_required)
+    def post(self, request, community_id):
+        community = get_object_or_404(Community, id=community_id)
+        community.members.remove(request.user)
+        return redirect(reverse('osmcal.community:community', args=[community_id]))
