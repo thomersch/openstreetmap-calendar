fa25a8c04b56e6a544f8f55554ce70d30c26f946
Thomas Skowron <th@skowron.eu>
2021-12-08 15:02:40 +0100

    event: schema.org tag #18

---
+++ osmcal/templatetags/schema.py
@@ -0,0 +1,36 @@
+import json
+
+from django import template
+from django.utils.safestring import mark_safe
+from osmcal.models import Event
+
+register = template.Library()
+
+@register.simple_tag()
+def schema_block(evt):
+	if not isinstance(evt, Event):
+		raise TypeError("parameter needs to be an event")
+	data = {
+		"@context": "https://schema.org",
+		"@type": "Event",
+		"startDate": evt.start.isoformat(),
+		"name": evt.name
+	}
+	if evt.end:
+		data["endDate"] = evt.end.isoformat()
+
+	if evt.cancelled:
+		data["eventStatus"] = "https://schema.org/EventCancelled"
+	else:
+		data["eventStatus"] = "https://schema.org/EventScheduled"
+
+	if evt.location:
+		data["location"] = {
+			"latitude": evt.location.y,
+			"longitude": evt.location.x,
+		}
+
+	if evt.link:
+		data["url"] = evt.link
+
+	return mark_safe("""<script type="application/ld+json">{}</script>""".format(json.dumps(data)))
