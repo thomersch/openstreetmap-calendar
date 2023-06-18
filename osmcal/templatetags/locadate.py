ccfe97c96098e59534086c6a9c095e40938680ee
Thomas Skowron <th@skowron.eu>
2023-06-18 13:17:22 +0200

    chore: update deps and check formatting

--- osmcal/templatetags/locadate.py
+++ osmcal/templatetags/locadate.py
@@ -14,7 +14,8 @@ def loca_day_month_fmt(lang):
     fmt_str = gettext("day_month_format")
     if fmt_str == "PLACEHOLDER":
         """We're hacking around the fallback language here:
-        jS F would be 1st May in en, but in everything else we'd have 1st Май, which doesn't make any sense, so we have a placeholder in the translation and going to fallback to j E which is reasonable in most languages."""
+        jS F would be 1st May in en, but in everything else we'd have 1st Май, which doesn't make any sense, so we have a placeholder in the translation and going to fallback to j E which is reasonable in most languages.
+        """
         if lang.startswith("en"):
             return "jS F"
         else:
