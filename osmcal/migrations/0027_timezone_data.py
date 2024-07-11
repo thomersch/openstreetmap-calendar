9951250de617e161f7798146977dbb0fd5681da7
Martijn van Exel <m@rtijn.org>
2024-07-11 11:26:41 -0600

    replace timezonefinder with tzfpy the timezonefinder package [does not seem to be maintained anymore](https://github.com/jannikmi/timezonefinder/tree/master?tab=readme-ov-file#notice-looking-for-maintainers-reach-out-if-you-want-to-contribute). Moreover, it introduces numpy as a transient dependency, which seems heavy-handed.

--- osmcal/migrations/0027_timezone_data.py
+++ osmcal/migrations/0027_timezone_data.py
@@ -1,19 +1,18 @@
 from django.db import migrations
 from pytz import timezone
-from timezonefinder import TimezoneFinder
+from tzfpy import get_tz
 
 
 def set_timezones(apps, schema_editor):
-    tf = TimezoneFinder()
-    Event = apps.get_model('osmcal', 'Event')
+    Event = apps.get_model("osmcal", "Event")
     for event in Event.objects.filter(timezone=None):
         if event.location:
-            tz = tf.timezone_at(lng=event.location.x, lat=event.location.y)
+            tz = get_tz(event.location.x, event.location.y)
             if tz is not None:
                 event.timezone = tz
         if not event.timezone:
             # Either time zone could not be determined from location or no location is available.
-            event.timezone = 'UTC'
+            event.timezone = "UTC"
         tz = timezone(event.timezone)
         event.start = tz.localize(event.start.replace(tzinfo=None))
 
@@ -24,10 +23,6 @@ def set_timezones(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('osmcal', '0026_event_timezone'),
-    ]
+    dependencies = [("osmcal", "0026_event_timezone")]
 
-    operations = [
-        migrations.RunPython(set_timezones)
-    ]
+    operations = [migrations.RunPython(set_timezones)]
