83519ef02adc06f37c389ffba4290f17530fd146
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2024-09-24 09:22:27 +0200

    Update test_views.py to include radius parameter

--- osmcal/test_views.py
+++ osmcal/test_views.py
@@ -2,16 +2,10 @@ from django.test import Client, TestCase
 
 
 class EventListTest(TestCase):
-    def test_location_out_of_range_low(self):
+    def test_location_out_of_range(self):
         # Based on Sentry report OSM-CALENDAR-1W
         c = Client()
         resp = c.get("/events.ics?around=5564")  # The around parameter obviously doesn't make any sense.
-        self.assertEqual(resp.status_code, 400)
-
-    def test_location_out_of_range_high(self):
-        # Based on Sentry report OSM-CALENDAR-1W
-        c = Client()
-        resp = c.get("/events.ics?around=55,13,5,1")  # The around parameter obviously doesn't make any sense.
         self.assertEqual(resp.status_code, 400)
 
     def test_location_around_50k(self):
@@ -21,5 +15,5 @@ class EventListTest(TestCase):
 
     def test_location_around_dist(self):
         c = Client()
-        resp = c.get("/events.ics?around=52,13,5")
+        resp = c.get("/events.ics?around=52,13&around_radius=5")
         self.assertEqual(resp.status_code, 200)
