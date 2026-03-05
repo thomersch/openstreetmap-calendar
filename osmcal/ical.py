b8c416db801beaa2da2c21cf6e7556d22d9b32ac
Thomas Skowron <th@skowron.eu>
2026-03-05 16:09:25 +0100

    icalendar: switch to python icalendar package removing the home-grown one

--- osmcal/ical.py
+++ osmcal/ical.py
@@ -1,56 +1,61 @@
 from datetime import timedelta
-from textwrap import wrap
-from typing import Iterable, List
+from typing import Iterable
+
+import pytz
+from icalendar import Calendar, Event as VEvent, vDate, vGeo
 
 from .models import Event
 
 
+def _build_calendar() -> Calendar:
+    cal = Calendar()
+    cal.add("version", "2.0")
+    cal.add("prodid", "-//OSM Calendar")
+    return cal
+
+
+def _build_vevent(evt: Event) -> VEvent:
+    vevent = VEvent()
+    vevent.add("uid", "OSMCAL-{}".format(evt.id))
+    vevent.add("dtstamp", evt.start_localized)
+
+    if evt.cancelled:
+        vevent.add("status", "CANCELLED")
+
+    if evt.whole_day:
+        vevent.add("dtstart", vDate(evt.start_localized.date()))
+        if evt.end:
+            vevent.add("dtend", vDate((evt.end_localized + timedelta(days=1)).date()))
+    else:
+        tz = pytz.timezone(str(evt.timezone))
+        vevent.add("dtstart", evt.start_localized.astimezone(tz))
+        if evt.end:
+            vevent.add("dtend", evt.end_localized.astimezone(tz))
+
+    vevent.add("summary", evt.name)
+
+    description = evt.description or ""
+    if evt.link:
+        description += "\n" + "Event Website: " + evt.link
+    if description:
+        vevent.add("description", description)
+
+    if evt.location:
+        vevent.add("geo", vGeo((evt.location.y, evt.location.x)))
+    if evt.location_address:
+        vevent.add("location", evt.location_detailed_addr)
+
+    return vevent
+
+
 def encode_event(evt: Event) -> str:
-    lines = ["BEGIN:VCALENDAR", "VERSION:2.0", "PRODID:-//OSM Calendar"]
-    lines += event_body(evt)
-    lines += ["END:VCALENDAR"]
-    return "\r\n".join(map(line_format, lines)) + "\r\n"
+    cal = _build_calendar()
+    cal.add_component(_build_vevent(evt))
+    return cal.to_ical().decode("utf-8")
 
 
 def encode_events(evts: Iterable[Event]) -> str:
-    lines = ["BEGIN:VCALENDAR", "VERSION:2.0", "PRODID:-//OSM Calendar"]
+    cal = _build_calendar()
     for evt in evts:
-        lines += event_body(evt)
-    lines += ["END:VCALENDAR"]
-    return "\r\n".join(map(line_format, lines)) + "\r\n"
-
-
-def line_format(ln: str) -> str:
-    return "\r\n\t".join(wrap(ln.replace(",", "\\,").replace("\n", "\\n"), 72, drop_whitespace=False))
-
-
-def event_body(evt: Event) -> List[str]:
-    lines = ["BEGIN:VEVENT"]
-
-    lines.append("UID:OSMCAL-{}".format(evt.id))
-    lines.append("DTSTAMP:{:%Y%m%dT%H%M%S}".format(evt.start_localized))
-    if evt.cancelled:
-        lines.append("STATUS:CANCELLED")
-
-    if evt.whole_day:
-        lines.append("DTSTART;VALUE=DATE:{:%Y%m%d}".format(evt.start_localized))
-        if evt.end:
-            lines.append("DTEND;VALUE=DATE:{:%Y%m%d}".format(evt.end_localized + timedelta(days=1)))
-    else:
-        lines.append("DTSTART;TZID={}:{:%Y%m%dT%H%M%S}".format(evt.timezone, evt.start_localized))
-        if evt.end:
-            lines.append("DTEND;TZID={}:{:%Y%m%dT%H%M%S}".format(evt.timezone, evt.end_localized))
-
-    lines.append("SUMMARY:{}".format(evt.name))
-
-    description = evt.description
-    if evt.link:
-        description += "\n" + "Event Website: " + evt.link
-
-    if description:
-        lines.append("DESCRIPTION:{}".format(description))
-    if evt.location:
-        lines.append("GEO:{};{}".format(evt.location.x, evt.location.y))
-    if evt.location_address:
-        lines.append("LOCATION:{}".format(evt.location_detailed_addr))
-    return lines + ["END:VEVENT"]
+        cal.add_component(_build_vevent(evt))
+    return cal.to_ical().decode("utf-8")
